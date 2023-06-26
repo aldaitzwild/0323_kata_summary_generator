d3c39f2e576057c52ea01637b9cdefc4385cc2fd
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-06-26 08:49:53 +0200

    Add the kata

---
+++ kata.php
@@ -0,0 +1,417 @@
+<?php
+/**
+ * Construisez le sommaire qui correspond au tableau suivant.
+ * Chaque chapitre doit suivre le modèle suivant (titre sur 50 caractère à compléter
+ * par des points, puis le niméro de page):
+ * 
+ * purus. Nullam scelerisque neque................... 59
+ * sollicitudin a, malesuada id,..................... 82
+ * mollis. Duis sit amet............................. 143
+ * ...
+ * ...
+ * 
+ * Naturellement les chapitres doivent apparaitre dans l'ordre de leur page :-)
+ */
+
+$book = [
+	[
+		"chapter" => "purus. Nullam scelerisque neque",
+		"page" => "59"
+	],
+	[
+		"chapter" => "sollicitudin a, malesuada id,",
+		"page" => "82"
+	],
+	[
+		"chapter" => "mollis. Duis sit amet",
+		"page" => "143"
+	],
+	[
+		"chapter" => "amet metus. Aliquam erat",
+		"page" => "2"
+	],
+	[
+		"chapter" => "odio a purus. Duis",
+		"page" => "42"
+	],
+	[
+		"chapter" => "sollicitudin commodo ipsum. Suspendisse",
+		"page" => "211"
+	],
+	[
+		"chapter" => "ligula consectetuer rhoncus. Nullam",
+		"page" => "93"
+	],
+	[
+		"chapter" => "neque et nunc. Quisque",
+		"page" => "291"
+	],
+	[
+		"chapter" => "elit, pretium et, rutrum",
+		"page" => "258"
+	],
+	[
+		"chapter" => "metus. Aenean sed pede",
+		"page" => "194"
+	],
+	[
+		"chapter" => "enim diam vel arcu.",
+		"page" => "339"
+	],
+	[
+		"chapter" => "cursus a, enim. Suspendisse",
+		"page" => "231"
+	],
+	[
+		"chapter" => "cursus purus. Nullam scelerisque",
+		"page" => "210"
+	],
+	[
+		"chapter" => "at, nisi. Cum sociis",
+		"page" => "118"
+	],
+	[
+		"chapter" => "sagittis. Duis gravida. Praesent",
+		"page" => "263"
+	],
+	[
+		"chapter" => "euismod urna. Nullam lobortis",
+		"page" => "332"
+	],
+	[
+		"chapter" => "quam quis diam. Pellentesque",
+		"page" => "172"
+	],
+	[
+		"chapter" => "vel quam dignissim pharetra.",
+		"page" => "136"
+	],
+	[
+		"chapter" => "sagittis. Nullam vitae diam.",
+		"page" => "231"
+	],
+	[
+		"chapter" => "dolor. Fusce feugiat. Lorem",
+		"page" => "43"
+	],
+	[
+		"chapter" => "magna. Suspendisse tristique neque",
+		"page" => "81"
+	],
+	[
+		"chapter" => "Duis cursus, diam at",
+		"page" => "334"
+	],
+	[
+		"chapter" => "ipsum. Phasellus vitae mauris",
+		"page" => "19"
+	],
+	[
+		"chapter" => "interdum feugiat. Sed nec",
+		"page" => "39"
+	],
+	[
+		"chapter" => "cubilia Curae Phasellus ornare.",
+		"page" => "334"
+	],
+	[
+		"chapter" => "Vivamus euismod urna. Nullam",
+		"page" => "315"
+	],
+	[
+		"chapter" => "mattis ornare, lectus ante",
+		"page" => "218"
+	],
+	[
+		"chapter" => "mauris ipsum porta elit,",
+		"page" => "311"
+	],
+	[
+		"chapter" => "dolor dolor, tempus non,",
+		"page" => "31"
+	],
+	[
+		"chapter" => "tellus lorem eu metus.",
+		"page" => "293"
+	],
+	[
+		"chapter" => "Phasellus fermentum convallis ligula.",
+		"page" => "98"
+	],
+	[
+		"chapter" => "mi enim, condimentum eget,",
+		"page" => "45"
+	],
+	[
+		"chapter" => "Nulla eu neque pellentesque",
+		"page" => "331"
+	],
+	[
+		"chapter" => "tempus risus. Donec egestas.",
+		"page" => "301"
+	],
+	[
+		"chapter" => "Nunc sed orci lobortis",
+		"page" => "349"
+	],
+	[
+		"chapter" => "pede sagittis augue, eu",
+		"page" => "314"
+	],
+	[
+		"chapter" => "sagittis felis. Donec tempor,",
+		"page" => "259"
+	],
+	[
+		"chapter" => "tellus eu augue porttitor",
+		"page" => "215"
+	],
+	[
+		"chapter" => "facilisis facilisis, magna tellus",
+		"page" => "110"
+	],
+	[
+		"chapter" => "vulputate, posuere vulputate, lacus.",
+		"page" => "304"
+	],
+	[
+		"chapter" => "hendrerit. Donec porttitor tellus",
+		"page" => "228"
+	],
+	[
+		"chapter" => "ornare lectus justo eu",
+		"page" => "83"
+	],
+	[
+		"chapter" => "ac risus. Morbi metus.",
+		"page" => "271"
+	],
+	[
+		"chapter" => "lorem, vehicula et, rutrum",
+		"page" => "169"
+	],
+	[
+		"chapter" => "euismod ac, fermentum vel,",
+		"page" => "60"
+	],
+	[
+		"chapter" => "enim diam vel arcu.",
+		"page" => "237"
+	],
+	[
+		"chapter" => "consectetuer adipiscing elit. Curabitur",
+		"page" => "306"
+	],
+	[
+		"chapter" => "malesuada vel, venenatis vel,",
+		"page" => "44"
+	],
+	[
+		"chapter" => "Aliquam ultrices iaculis odio.",
+		"page" => "215"
+	],
+	[
+		"chapter" => "pharetra, felis eget varius",
+		"page" => "94"
+	],
+	[
+		"chapter" => "volutpat nunc sit amet",
+		"page" => "148"
+	],
+	[
+		"chapter" => "in, cursus et, eros.",
+		"page" => "136"
+	],
+	[
+		"chapter" => "adipiscing lacus. Ut nec",
+		"page" => "151"
+	],
+	[
+		"chapter" => "in lobortis tellus justo",
+		"page" => "227"
+	],
+	[
+		"chapter" => "nascetur ridiculus mus. Proin",
+		"page" => "48"
+	],
+	[
+		"chapter" => "euismod enim. Etiam gravida",
+		"page" => "107"
+	],
+	[
+		"chapter" => "Sed dictum. Proin eget",
+		"page" => "184"
+	],
+	[
+		"chapter" => "non enim. Mauris quis",
+		"page" => "259"
+	],
+	[
+		"chapter" => "erat eget ipsum. Suspendisse",
+		"page" => "21"
+	],
+	[
+		"chapter" => "pede et risus. Quisque",
+		"page" => "336"
+	],
+	[
+		"chapter" => "amet risus. Donec egestas.",
+		"page" => "285"
+	],
+	[
+		"chapter" => "accumsan convallis, ante lectus",
+		"page" => "303"
+	],
+	[
+		"chapter" => "Vestibulum ut eros non",
+		"page" => "324"
+	],
+	[
+		"chapter" => "mauris a nunc. In",
+		"page" => "217"
+	],
+	[
+		"chapter" => "tempus risus. Donec egestas.",
+		"page" => "86"
+	],
+	[
+		"chapter" => "arcu imperdiet ullamcorper. Duis",
+		"page" => "311"
+	],
+	[
+		"chapter" => "lorem fringilla ornare placerat,",
+		"page" => "61"
+	],
+	[
+		"chapter" => "porttitor vulputate, posuere vulputate,",
+		"page" => "338"
+	],
+	[
+		"chapter" => "ullamcorper. Duis cursus, diam",
+		"page" => "198"
+	],
+	[
+		"chapter" => "imperdiet non, vestibulum nec,",
+		"page" => "203"
+	],
+	[
+		"chapter" => "nunc sed libero. Proin",
+		"page" => "143"
+	],
+	[
+		"chapter" => "magnis dis parturient montes,",
+		"page" => "84"
+	],
+	[
+		"chapter" => "Cras sed leo. Cras",
+		"page" => "247"
+	],
+	[
+		"chapter" => "mauris elit, dictum eu,",
+		"page" => "110"
+	],
+	[
+		"chapter" => "ornare, libero at auctor",
+		"page" => "73"
+	],
+	[
+		"chapter" => "sollicitudin orci sem eget",
+		"page" => "273"
+	],
+	[
+		"chapter" => "Nulla facilisi. Sed neque.",
+		"page" => "108"
+	],
+	[
+		"chapter" => "non magna. Nam ligula",
+		"page" => "151"
+	],
+	[
+		"chapter" => "semper erat, in consectetuer",
+		"page" => "326"
+	],
+	[
+		"chapter" => "mi lacinia mattis. Integer",
+		"page" => "290"
+	],
+	[
+		"chapter" => "cubilia Curae Donec tincidunt.",
+		"page" => "201"
+	],
+	[
+		"chapter" => "vitae risus. Duis a",
+		"page" => "255"
+	],
+	[
+		"chapter" => "adipiscing lobortis risus. In",
+		"page" => "65"
+	],
+	[
+		"chapter" => "primis in faucibus orci",
+		"page" => "37"
+	],
+	[
+		"chapter" => "Donec egestas. Aliquam nec",
+		"page" => "211"
+	],
+	[
+		"chapter" => "sed sem egestas blandit.",
+		"page" => "107"
+	],
+	[
+		"chapter" => "sit amet diam eu",
+		"page" => "337"
+	],
+	[
+		"chapter" => "quam a felis ullamcorper",
+		"page" => "239"
+	],
+	[
+		"chapter" => "eget mollis lectus pede",
+		"page" => "42"
+	],
+	[
+		"chapter" => "tristique aliquet. Phasellus fermentum",
+		"page" => "229"
+	],
+	[
+		"chapter" => "neque vitae semper egestas,",
+		"page" => "174"
+	],
+	[
+		"chapter" => "non, egestas a, dui.",
+		"page" => "340"
+	],
+	[
+		"chapter" => "felis, adipiscing fringilla, porttitor",
+		"page" => "40"
+	],
+	[
+		"chapter" => "dictum. Phasellus in felis.",
+		"page" => "203"
+	],
+	[
+		"chapter" => "malesuada fames ac turpis",
+		"page" => "152"
+	],
+	[
+		"chapter" => "Morbi vehicula. Pellentesque tincidunt",
+		"page" => "101"
+	],
+	[
+		"chapter" => "felis orci, adipiscing non,",
+		"page" => "6"
+	],
+	[
+		"chapter" => "faucibus orci luctus et",
+		"page" => "285"
+	],
+	[
+		"chapter" => "ac, eleifend vitae, erat.",
+		"page" => "296"
+	],
+	[
+		"chapter" => "Nullam lobortis quam a",
+		"page" => "289"
+	]
+];
